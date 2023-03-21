18ebf1f1bfb55eedf3462e664c3548f5913a8e17
timothy <timothykerong@gmail.com>
2023-03-21 23:15:55 +0800

    update listing table

--- routes/listings.js
+++ routes/listings.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const db = require('../database/db')
 
 router.get("/", (req, res) => {
-    db.query("SELECT * FROM listing", function (err, result, fields) {
+    db.query("SELECT * FROM listing ORDER BY ID DESC", function (err, result, fields) {
         if (err) throw err;
         return res.send(result);
     });
 });
 
 router.post("/", (req, res) => {
-    const { title, picture, description, starttime, endtime, location, portionremaining } = req.body
-    const query = `INSERT INTO listing (title, picture, description,  starttime, endtime, location,  portionremaining) VALUES ('${title}', '${picture}', '${description}',  '${starttime}', '${endtime}', '${location}',  '${portionremaining}');`
+    const { title, picture, description, endtime, date, location, portionremaining } = req.body
+    console.log(location);
+    // console.log(title, picture, description, endtime, date, location, portionremaining);
+    const query = `INSERT INTO listing (title, picture, description, endtime, date, location,  portionremaining) VALUES ('${title}', '${picture}', '${description}', '${endtime}', '${date}', '${location}',  '${portionremaining}');`
     db.query(query, function (err, result, fields) {
         if (err) throw err;
         return res.send(result);
@@ -20,8 +22,8 @@ router.post("/", (req, res) => {
 
 
 router.put("/", (req, res) => {
-    const { id, title, picture, description, starttime, endtime, location, portionremaining } = req.body
-    const query = `UPDATE listing SET title = '${title}', picture = '${picture}', description = '${description}',  starttime = '${starttime}', endtime= '${endtime}', location = '${location}',  portionremaining = '${portionremaining}' WHERE id = ${id};`
+    const { id, title, picture, description, endtime, date, location, portionremaining } = req.body
+    const query = `UPDATE listing SET title = '${title}', picture = '${picture}', description = '${description}',  endtime= '${endtime}', date = '${date}', location = '${location}',  portionremaining = '${portionremaining}' WHERE id = ${id};`
     db.query(query, function (err, result, fields) {
         if (err) throw err;
         return res.send(result);
