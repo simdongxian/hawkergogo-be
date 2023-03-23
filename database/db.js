baef9f484157e35c9917bc99c6fbc08623fd65da
timothy huang <krhuang.2021@scis.smu.edu.sg>
2023-03-23 16:04:37 +0800

    update db init data

--- database/db.js
+++ database/db.js
@@ -11,7 +11,7 @@ const database = () => {
 
     const listingTable = `CREATE TABLE IF NOT EXISTS listing (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(50), picture VARCHAR(200), description VARCHAR(50), endtime VARCHAR(50), date VARCHAR(50), location VARCHAR(50),  portionremaining VARCHAR(50), PRIMARY KEY(id) );`;
     const cartTable = `CREATE TABLE IF NOT EXISTS cart (listing_id INT, portion INT, PRIMARY KEY(listing_id) );`;
-    const query = "INSERT INTO listing (title, picture, description, endtime, date, location,  portionremaining) SELECT * FROM (SELECT 'Chicken Rice', 'https://singaporelocalfavourites.com/wp-content/uploads/2017/11/singapore-hainanese-roasted-chicken-rice.jpg', 'Delicious chick rice', '22:00', '2023-03-21', 'Prinsep Street', '5') AS FOO  WHERE NOT EXISTS (SELECT * FROM listing);"
+    const query = "INSERT INTO listing (title, picture, description, endtime, date, location,  portionremaining) SELECT * FROM (SELECT 'Chicken Rice', 'food_chickenrice', 'Delicious chick rice', '22:00', '2023-03-21', 'Prinsep Street', '5') AS FOO  WHERE NOT EXISTS (SELECT * FROM listing);"
 
     db.connect(function (err) {
         if (err) throw err;
